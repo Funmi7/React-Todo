f018877aaa12b187217d29ec455eecbebfb75773
funmi7 <talabifunmilayo@gmail.com>
2020-09-05 00:08:27 +0100

    Styled to do app

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import './Todo.css';
+import React from "react";
+import "./Todo.css";
+import styled from "styled-components";
 
+const TodoStyled = styled.div`
+  font-size: 1.2rem;
+`;
 
-class Todo extends React.Component{
-    render() {
-        const {todo, onTaskComplete} = this.props;
-        return(
-            <div
-            className={`${this.props.todo.completed? 'finished-task': null}`}
-            onClick={() => onTaskComplete(this.props.todo.id)}
-            >
-            {todo.task}
-            </div>
-        )
-    }
+class Todo extends React.Component {
+  render() {
+    const { todo, onTaskComplete } = this.props;
+    return (
+      <TodoStyled
+        className={`${this.props.todo.completed ? "finished-task" : null}`}
+        onClick={() => onTaskComplete(this.props.todo.id)}
+      >
+        {todo.task}
+      </TodoStyled>
+    );
+  }
 }
 
 export default Todo;
